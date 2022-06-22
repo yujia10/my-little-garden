2f067c540df4bb006e5d23052f17e6c5377d7504
Yujia Gu <yujiagu1010@gmail.com>
2022-06-22 15:57:12 +0930

    add css

--- src/List.js
+++ src/List.js
@@ -4,10 +4,10 @@ const List = ({items})=>{
   return (
     <div className='section-center'>
       {items.map((item)=>{
-        const {id, title, image } = item;
+        const {id, title, img } = item;
         return (
           <article key={id} className='plant-item'>
-            <img src={image} alt={title} className='photo'></img>
+            <img src={img} alt={title} className='photo'></img>
             <div className='item-info'>
               <h4>{title}</h4>
               <p></p>
