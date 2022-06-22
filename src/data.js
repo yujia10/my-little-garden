e6f3371e0e515d8b6ca4e3365c617165dc835953
Yujia Gu <yujiagu1010@gmail.com>
2022-06-22 14:30:38 +0930

    initial commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const plants = [
+  {
+    id:1,
+    title:"ECHEVERIA 'CRYSTAL ROSE' | 水晶玫瑰",
+    category:"baby plants",
+    img:'./images/item-1.jpg',
+    watered: false,
+  },
+  {
+    id:2,
+    title:"ECHEVERIA 'ICE GREEN' | 冰玉",
+    category:"baby plants",
+    img:'./images/item-2.jpg',
+    watered: false,
+  },
+  {
+    id:3,
+    title:"ECHEVERIA 'CONCH GIRL' ｜ 海螺姑娘",
+    category:"baby plants",
+    img:'./images/item-3.jpg',
+    watered: false,
+  },
+  {
+    id:4,
+    title:"ECHEVERIA Alba beauty hybrid ｜ 如梦令",
+    category:"mature plants",
+    img:'./images/item-4.jpg',
+    watered: false,
+  },
+  {
+    id:5,
+    title:"ECHEVERIA 'CHEERY' | 樱桃",
+    category:"mature plants",
+    img:'./images/item-5.jpg',
+    watered: false,
+  },
+  {
+    id:6,
+    title:"ECHEVERIA 'NEON BREAKER' | 晚霞之舞",
+    category:"mature plants",
+    img:'./images/item-6.jpg',
+    watered: false,
+  },
+  {
+    id:7,
+    title:"ECHEVERIA 'CREAM ROSE' | 奶油玫瑰",
+    category:"mature plants",
+    img:'./images/item-7.jpg',
+    watered: false,
+  },
+];
+export default plants;
